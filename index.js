a1ba82d2bedf237e5967bab3813a97754e1fdf3c
Ryan Slater <ryanryanslater@gmail.com>
2024-12-26 17:40:10 +1000

    list of commands

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ process.stdin.on('keypress', async (chunk, key) => {
   if (key && key.name == "return") {
     await drone.sendCommand('command');
   }
-  
+
   if (key && key.name == "space") {
     await drone.sendCommand('takeoff');
   }
@@ -31,4 +31,19 @@ process.stdin.on('keypress', async (chunk, key) => {
   console.log(key.name)
 });
 
-
+/**
+ * command
+ * takeoff
+ * delay 5
+ * land
+ * streamon
+ * streamoff
+ * move, format(direction, x: 20-500 cm)
+ * up, x: 20-500 cm
+ * down, x: 20-500 cm
+ * left, x: 20-500 cm
+ * right, x: 20-500 cm
+ * forward, x: 20-500 cm
+ * back, x: 20-500 cm
+ * flip, x: l, r, f, b
+ */
